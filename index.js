de80c80b1f605e59c753a4591127d13564c2dc3f
Jocelyn Naarden <tzionone@gmail.com>
2022-04-18 01:50:03 -0400

    🏆🏆Finished Assignment🏆🏆

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = '🏆Jocelyn is the champion🏆'
+newHeader.style = "font-family: sans-serif; color: #8E562E;"
+document.body.append(newHeader)
+document.body.style = "background-color: #FFD679"
+
+console.log(main)
